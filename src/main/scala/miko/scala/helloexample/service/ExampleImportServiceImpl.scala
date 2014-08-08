476ba54b5334212a0a9a039dbe38379f130375e0
Miroslav Kopecky <kopeckymiroslav@gmail.com>
2014-08-08 22:06:53 +0200

    Spring 4 Scala working Init Config without Web.XML

--- src/main/scala/miko/scala/helloexample/service/ExampleImportServiceImpl.scala
+++ src/main/scala/miko/scala/helloexample/service/ExampleImportServiceImpl.scala
@@ -1,8 +1,11 @@
 package miko.scala.helloexample.service
 
+import org.springframework.stereotype.Service
+
 /**
  * @author kopeckymiroslav
  */
+@Service("exampleImportService")
 class ExampleImportServiceImpl extends ExampleImportService{
   override def name: String = "Imported service over XML"
 }
