104e09c42d9bbf1d2d422b10cbec191ef2aaae52
Miroslav Kopecky <kopeckymiroslav@gmail.com>
2014-08-09 22:44:05 +0200

    slf4j and log4j 1.x update

--- build.sbt
+++ build.sbt
@@ -10,6 +10,14 @@ libraryDependencies ++= Seq(
   "org.springframework" % "spring-context-support" % "4.0.6.RELEASE",
   "javax.servlet" % "javax.servlet-api" % "3.0.1" % "provided",
   "javax.servlet" % "jstl" % "1.2" % "compile",
+  "org.slf4j" % "jcl-over-slf4j" % "1.7.5" ,
+  "org.slf4j" % "slf4j-api" % "1.7.5" ,
+  "org.slf4j" % "slf4j-log4j12" % "1.7.5",
+  "log4j" % "log4j" % "1.2.17" excludeAll(
+        ExclusionRule(organization = "com.sun.jdmk"),
+        ExclusionRule(organization = "com.sun.jmx"),
+        ExclusionRule(organization = "javax.jms")
+      ),
   "org.eclipse.jetty" % "jetty-webapp" % "9.1.0.v20131115" % "container, compile",
   "org.eclipse.jetty" % "jetty-jsp" % "9.1.0.v20131115" % "container"
 )
--- src/main/scala/miko/scala/helloexample/controller/HelloExampleController.scala
+++ src/main/scala/miko/scala/helloexample/controller/HelloExampleController.scala
@@ -1,6 +1,7 @@
 package miko.scala.helloexample.controller
 
 import miko.scala.helloexample.service.{ExampleImportService, ExampleService}
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -14,8 +15,15 @@ import org.springframework.web.bind.annotation.{RequestMethod, RequestMapping}
 @RequestMapping(Array("/"))
 class HelloExampleController @Autowired() (exampleService: ExampleService, exampleImportService: ExampleImportService){
 
+  private val logger = LoggerFactory.getLogger(getClass)
+
   @RequestMapping(method =  Array(RequestMethod.GET))
   def hello( model: Model) = {
+
+    logger.info("Info Test")
+    logger.debug("Debug Test")
+    logger.error("Error Test")
+
     model.addAttribute("message", "Here is Scala with ServiceName = " + exampleService.serviceName)
     model.addAttribute("importMessage", "Here is Scala with ServiceImportName = " + exampleImportService.name)
     "hello"
